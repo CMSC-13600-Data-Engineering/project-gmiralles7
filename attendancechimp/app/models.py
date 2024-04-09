a199ba81b9f296d9a6de83e365429d72c7a20680
gmiralles7 <117242918+gmiralles7@users.noreply.github.com>
2024-04-08 23:28:42 -0500

    Update models.py

--- attendancechimp/app/models.py
+++ attendancechimp/app/models.py
@@ -6,26 +6,4 @@ class Course(models.Model):
     number = models.CharField(max_length=5)
 
     
-class Student(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    number = models.CharField(max_length=8)
 
-class Instructor(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    number = models.CharField(max_length=5)
-
-class Lecture(models.Model):
-    QR= models.IntegerField(primary_key=True) #QR ID for each Lecture of Each Course
-    day= models.DateField()
-    course=models.ForeignKey(Course, on_delete=models.CASCADE)
-
-
-class Enrollment(models.Model):
-    course=models.ForeignKey(Course, on_delete=models.CASCADE)
-    student=models.ForeignKey(Student, on_delete=models.CASCADE)
-
-class Attendance(models.Model):
-    QR=models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    student=models.ForeignKey(Student, on_delete=models.CASCADE)
-    
-    
